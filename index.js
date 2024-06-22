4465941459321f09f9269a48cdbc425fbfa1b673
SahilUmmer <srk.sahilkhan@gmail.com>
2024-06-23 00:11:14 +0530

    completed exercise

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 const express = require('express')
-const app = express()
+const morgan = require('morgan')
 
-let persons= [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+const app = express()
+app.use(express.json())
+
+morgan.token('method', function (req) { return req.method; });
+morgan.token('url', function (req) { return req.originalUrl; });
+morgan.token('status', function (req, res) { return res.statusCode; });
+morgan.token('content_length', function (req, res) { return res.get('Content-Length') || 0; });
+morgan.token('response_time', function (req, res) { return new Date() - req._startTime; });
+morgan.token('data', function (req) { return JSON.stringify(req.body); });
+
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.content_length(req, res), '-', // Consistent dot notation
+        tokens.response_time(req, res), 'ms', // Consistent dot notation
+        tokens.data(req, res)
+    ].join(' ');
+}));
+
+let persons = [
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ];
 
@@ -28,24 +49,42 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-app.get("/api/persons" ,(request, response) => {
+app.get("/api/persons", (request, response) => {
     response.json(persons);
 });
 
-app.get("/api/persons/:id" ,(request, response) => {
+app.get("/api/persons/:id", (request, response) => {
     const personId = new Number(request.params.id);
     const person = persons.filter(currentPerson => currentPerson.id = personId);
 
-    return person? response.send(person) : response.status(502).send(`Person ${personId} not found`);
+    return person ? response.send(person) : response.status(502).send(`Person ${personId} not found`);
 });
 
-app.delete("/api/persons/:id" ,(request, response) => {
+app.post("/api/persons", (request, response) => {
+    if (!request.body) {
+        response.status(400).end();
+    } else {
+        const person = request.body;
+        console.log(person);
+        let max = 0;
+        persons.forEach(element => {
+            max = Math.max(max, element.id);
+        });
+        max++;
+        person.id = max;
+        persons.push(person);
+        response.json(person);
+    }
+
+});
+
+app.delete("/api/persons/:id", (request, response) => {
     const personId = new Number(request.params.id);
     const person = persons.filter(currentPerson => currentPerson.id == personId);
 
-    if(person && person.length > 0){
+    if (person && person.length > 0) {
         console.log("person found", person);
-        persons = persons.filter( currentPerson => personId != currentPerson.id);
+        persons = persons.filter(currentPerson => personId != currentPerson.id);
         response.send(person);
     } else {
         console.log("person not found");
