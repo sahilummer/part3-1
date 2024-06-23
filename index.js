d242048d886717924cc50a148ab8ef8aa0119723
SahilUmmer <srk.sahilkhan@gmail.com>
2024-06-23 14:56:43 +0530

    Cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors');
 
 const app = express()
+
+app.use(cors());
 app.use(express.json())
 
 morgan.token('method', function (req) { return req.method; });
@@ -97,7 +100,7 @@ app.get("/info", (request, response) => {
     response.send(`Phonebook has ${persons.length} users.<br/>${new Date()} `);
 })
 
-const PORT = 3001
+const PORT =  process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
