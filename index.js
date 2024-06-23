74e53ebc56d8dc66e2454e8d4f7f89c81ae16bd0
SahilUmmer <srk.sahilkhan@gmail.com>
2024-06-23 17:10:28 +0530

    React front end

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(cors());
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('method', function (req) { return req.method; });
 morgan.token('url', function (req) { return req.originalUrl; });
